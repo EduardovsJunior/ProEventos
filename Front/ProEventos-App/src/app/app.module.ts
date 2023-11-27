a40c2529f885fb83141f3e480e41821ba94e914b
EduardoJr <evsantosjunior@gmail.com>
2023-11-27 07:55:08 -0300

    Seção 7 - Organizar, rota, alertas e mais

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './shared/nav/nav.component';
+import {TitleComponent} from "./shared/title/title.component"
 
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { FormsModule } from '@angular/forms';
@@ -17,6 +18,9 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
 
 
@@ -26,7 +30,12 @@ import { NgxSpinnerModule } from 'ngx-spinner';
       EventosComponent,
       PalestrantesComponent,
       NavComponent,
-      DateTimeFormatePipe
+      DateTimeFormatePipe,
+      TitleComponent,
+      ContatosComponent,
+      DashboardComponent,
+      PerfilComponent
+    
    ],
   imports: [
     BrowserModule,
